d3873e8787aeed5b6cf48e17051e34f36b196d45
Kyrre Sjobak <k.n.sjobak@fys.uio.no>
2015-08-19 15:42:25 +0200

    Analyzer improvements

--- analyzer.py
+++ analyzer.py
@@ -1,10 +1,12 @@
-import ROOT, ROOT.gPad
+import ROOT#, ROOT.gPad
 import sys
 
 names = []
 files = []
 
+protonAngleHistos = []
 protonEnergyHistos = []
+targetEdep_IELHistos = []
 
 for arg in sys.argv[1:]:
     print "Reading file '"+arg+"'..."
@@ -17,16 +19,11 @@ for arg in sys.argv[1:]:
     # Protons with dpp/p < 90%
     protonAngle = files[-1].Get("protonAngle")
     protonEnergy = files[-1].Get("protonEnergy")
-    
-    protonEnergyHistos.append(protonEnergy)
+    targetEdep_IEL = files[-1].Get("targetEdep_IEL")
 
-    # c1 = ROOT.TCanvas()
-    # protonAngle.Draw()
-    # ROOT.gPad.SetLogy()
-    # c2 = ROOT.TCanvas()
-    # protonEnergy.Draw()
-    # ROOT.gPad.SetLogy()
-    # raw_input()
+    protonAngleHistos.append(protonAngle)
+    protonEnergyHistos.append(protonEnergy)
+    targetEdep_IELHistos.append(targetEdep_IEL)
     
     print
     print "Integrals for protonEnergy:"
@@ -36,10 +33,13 @@ for arg in sys.argv[1:]:
     upperE1_bin = xaxis.FindBin(upperE1)
     upperE2 = beamE*0.8
     upperE2_bin = xaxis.FindBin(upperE2)
-    lowE = 0.01
-    lowE_bin = xaxis.FindBin(lowE)
+
     zeroE  = 0.0
     zeroE_bin = xaxis.FindBin(zeroE)
+    lowE1 = 0.01
+    lowE1_bin = xaxis.FindBin(lowE1)
+    lowE2 = 1.0
+    lowE2_bin = xaxis.FindBin(lowE2)
     
     print "Int0:"
     print "\t Range: all"
@@ -57,37 +57,59 @@ for arg in sys.argv[1:]:
     
     print "Int2:"
     print "\t Range: {0}:{1} [TeV] -> {2}:{3} [{4}:{5}]".format(
-        lowE,upperE1,
-        xaxis.GetBinLowEdge(lowE_bin),xaxis.GetBinUpEdge(upperE1_bin),
-        lowE_bin,upperE1_bin
+        lowE1,upperE1,
+        xaxis.GetBinLowEdge(lowE1_bin),xaxis.GetBinUpEdge(upperE1_bin),
+        lowE1_bin,upperE1_bin
     )
-    int2 = protonEnergy.Integral(lowE_bin,upperE1_bin)
+    int2 = protonEnergy.Integral(lowE1_bin,upperE1_bin)
     print "\t int2=" + str(int2)
     
     print "Int3:"
     print "\t Range: {0}:{1} [TeV] -> {2}:{3} [{4}:{5}]".format(
-        lowE,upperE2,
-        xaxis.GetBinLowEdge(lowE_bin),xaxis.GetBinUpEdge(upperE2_bin),
-        lowE_bin,upperE2_bin
+        lowE1,upperE2,
+        xaxis.GetBinLowEdge(lowE1_bin),xaxis.GetBinUpEdge(upperE2_bin),
+        lowE1_bin,upperE2_bin
     )
-    int3 = protonEnergy.Integral(lowE_bin,upperE2_bin)
+    int3 = protonEnergy.Integral(lowE1_bin,upperE2_bin)
     print "\t int3=" + str(int3)
+
+    print "Int4:"
+    print "\t Range: {0}:{1} [TeV] -> {2}:{3} [{4}:{5}]".format(
+        lowE2,upperE2,
+        xaxis.GetBinLowEdge(lowE2_bin),xaxis.GetBinUpEdge(upperE2_bin),
+        lowE2_bin,upperE2_bin
+    )
+    int4 = protonEnergy.Integral(lowE2_bin,upperE2_bin)
+    print "\t int4=" + str(int4)
+
     
     print "Ratio int1/int0 =", int1/int0*100, "%"
     print "Ratio int2/int0 =", int2/int0*100, "%"
     print "Ratio int3/int0 =", int3/int0*100, "%"
+    print "Ratio int4/int0 =", int4/int0*100, "%"
 
 print
 print
 
-for n,h,idx in zip(names, protonEnergyHistos,xrange(len(names))):
-    hNew = None
-    if idx == 0:
-        hNew = h.DrawNormalized()
-    else:
-        hNew = h.DrawNormalized("SAME")
-    hNew.SetLineColor(idx+1)
+def plotHistos(histosList,doLog=True):
+    c = ROOT.TCanvas()
+    for n,h,idx in zip(names, histosList,xrange(len(names))):
+        if h.GetSumOfWeights()==0.0:
+            #Empty histogram
+            continue
+        hNew = None
+        if idx == 0:
+            hNew = h.DrawNormalized()
+        else:
+            hNew = h.DrawNormalized("SAME")
+        hNew.SetLineColor(idx+1)
+    if doLog:
+        ROOT.gPad.SetLogy()
+    return c;
 
-    #print idx,n,h
-ROOT.gPad.SetLogy()
+c1=plotHistos(protonAngleHistos)
+c2=plotHistos(protonEnergyHistos)
+c3=plotHistos(targetEdep_IELHistos,False)
 raw_input()
+
+
