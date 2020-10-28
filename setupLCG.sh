800f48db148a537c3625c4533fefc884d62d7ce1
Kyrre Sjobak <kyrre.ness.sjoebaek@cern.ch>
2020-10-28 15:42:30 +0100

    Update to LCG97python3

--- setupLCG.sh
+++ setupLCG.sh
@@ -4,19 +4,40 @@
 
 export LCGENV_PATH=/cvmfs/sft.cern.ch/lcg/releases
 
+LCGENV=/cvmfs/sft.cern.ch/lcg/releases/LCG_98python3/lcgenv/1.3.14/x86_64-centos7-gcc10-opt/lcgenv
+LCGREL=LCG_97python3
+#LCGPLAT=x86_64-centos7-gcc9-opt
+LCGPLAT=x86_64-centos7-clang8-opt
+
 #Put all the commands in a script `setupLCG.sh` which is then ran,
-# since running the commands breaks the Python2 environment which LCGenv is running on
-/cvmfs/sft.cern.ch/lcg/releases/LCG_94python3/lcgenv/1.3.6/x86_64-centos7-gcc7-opt/lcgenv -p LCG_94python3 x86_64-centos7-gcc7-opt gcc     >  setupLCG_runme.sh
+# since running the commands breaks the Python2 environment which LCGenv is running on (AT LEAST IN LCG94)
+echo "" > setupLCG_runme.sh
 
-/cvmfs/sft.cern.ch/lcg/releases/LCG_94python3/lcgenv/1.3.6/x86_64-centos7-gcc7-opt/lcgenv -p LCG_94python3 x86_64-centos7-gcc7-opt CMake   >> setupLCG_runme.sh
+#$LCGENV -p $LCGREL $LCGPLAT gcc    >> setupLCG_runme.sh
 
-/cvmfs/sft.cern.ch/lcg/releases/LCG_94python3/lcgenv/1.3.6/x86_64-centos7-gcc7-opt/lcgenv -p LCG_94python3 x86_64-centos7-gcc7-opt hdf5    >> setupLCG_runme.sh
+echo "# CMAKE:" >> setupLCG_runme.sh
+$LCGENV -p $LCGREL $LCGPLAT CMake   >> setupLCG_runme.sh
+echo "" >> setupLCG_runme.sh
+echo "" >> setupLCG_runme.sh
 
-/cvmfs/sft.cern.ch/lcg/releases/LCG_94python3/lcgenv/1.3.6/x86_64-centos7-gcc7-opt/lcgenv -p LCG_94python3 x86_64-centos7-gcc7-opt Geant4  >> setupLCG_runme.sh
-echo "source geant4.sh"                                                                                                                    >> setupLCG_runme.sh 
+echo "# CMAKE:" >> setupLCG_runme.sh
+$LCGENV -p $LCGREL $LCGPLAT hdf5    >> setupLCG_runme.sh
+echo "" >> setupLCG_runme.sh
+echo "" >> setupLCG_runme.sh
 
-/cvmfs/sft.cern.ch/lcg/releases/LCG_94python3/lcgenv/1.3.6/x86_64-centos7-gcc7-opt/lcgenv -p LCG_94python3 x86_64-centos7-gcc7-opt ROOT    >> setupLCG_runme.sh
+echo "# GEANT4:" >> setupLCG_runme.sh
+$LCGENV -p $LCGREL $LCGPLAT XercesC >> setupLCG_runme.sh
+$LCGENV -p $LCGREL $LCGPLAT Geant4  >> setupLCG_runme.sh
+echo "source geant4.sh"             >> setupLCG_runme.sh # From PATH
+echo "" >> setupLCG_runme.sh
+echo "" >> setupLCG_runme.sh
 
+echo "# ROOT:" >> setupLCG_runme.sh
+$LCGENV -p $LCGREL $LCGPLAT ROOT    >> setupLCG_runme.sh
+echo "" >> setupLCG_runme.sh
+echo "" >> setupLCG_runme.sh
+
+#RUN IT
 source setupLCG_runme.sh
 
 #Cleanup; comment out for debugging
