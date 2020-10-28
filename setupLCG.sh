1b59d7963246bb16da0f4809810f93fe719b6ba5
Kyrre Sjobak <kyrre.ness.sjoebaek@cern.ch>
2020-10-28 16:02:16 +0100

    Make the root dicts compile. Now works both with GCC and CLANG on LCG_97

--- setupLCG.sh
+++ setupLCG.sh
@@ -5,9 +5,13 @@
 export LCGENV_PATH=/cvmfs/sft.cern.ch/lcg/releases
 
 LCGENV=/cvmfs/sft.cern.ch/lcg/releases/LCG_98python3/lcgenv/1.3.14/x86_64-centos7-gcc10-opt/lcgenv
+
+#LCGREL=LCG_98python3
+#LCGPLAT=x86_64-centos7-gcc10-opt
+
 LCGREL=LCG_97python3
-#LCGPLAT=x86_64-centos7-gcc9-opt
-LCGPLAT=x86_64-centos7-clang8-opt
+LCGPLAT=x86_64-centos7-gcc9-opt
+#LCGPLAT=x86_64-centos7-clang8-opt
 
 #Put all the commands in a script `setupLCG.sh` which is then ran,
 # since running the commands breaks the Python2 environment which LCGenv is running on (AT LEAST IN LCG94)
