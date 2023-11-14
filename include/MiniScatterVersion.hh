4b10e378be4e8d850946999bf88bea8ab14e67b5
Kyrre Sjobak <k.n.sjobak@fys.uio.no>
2023-11-14 11:07:40 +0100

    Bump version to 1.3

--- include/MiniScatterVersion.hh
+++ include/MiniScatterVersion.hh
@@ -1,7 +1,7 @@
 #ifndef MINISCATTERVERSION_HH
 #define MINISCATTERVERSION_HH 1
 
-inline const char* const miniscatter_version = "1.2";
+inline const char* const miniscatter_version = "1.3";
 inline const char* const miniscatter_date    = "November 14th 2023";
 
 #endif
