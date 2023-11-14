bfd99703206457072e29b71ce4eb95781cbc5f41
Kyrre Sjobak <k.n.sjobak@fys.uio.no>
2023-11-14 16:33:42 +0100

    Updated all the notebooks

--- include/DetectorConstruction.hh
+++ include/DetectorConstruction.hh
@@ -107,11 +107,11 @@ private:
     G4Material*        gasAr                  = NULL;
 
     //These are all in G4 units
-    G4double           WorldSizeX;
+    G4double           WorldSizeX; // Full width of world
     G4double           WorldSizeY;
     G4double           WorldSizeZ;
 
-    G4double           TargetSizeX;
+    G4double           TargetSizeX; // Full width of target
     G4double           TargetSizeY;
     G4double           TargetThickness;
 
