717c5906817c8e52c394443f36bb11e8a59bb636
Kyrre Sjobak <kyrre.ness.sjoebaek@cern.ch>
2024-07-19 10:26:26 +0200

    Downgrade LCG to 105a, which is supported by SWAN

--- setupLCG.sh
+++ setupLCG.sh
@@ -2,7 +2,7 @@
 # capable of building and running MiniScatter
 # K. Sjobak, 11/02-2019
 
-LCGREL=LCG_106
+LCGREL=LCG_105a
 LCGPLAT=x86_64-el9-gcc13-opt
 
 source /cvmfs/sft.cern.ch/lcg/views/$LCGREL/$LCGPLAT/setup.sh
