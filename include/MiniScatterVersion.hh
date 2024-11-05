300c627aac52da45b3a143737c3f18acba26a28b
Kyrre Sjobak <kyrre.ness.sjoebaek@cern.ch>
2024-11-05 02:14:20 +0100

    Bump version to 1.3-4

--- include/MiniScatterVersion.hh
+++ include/MiniScatterVersion.hh
@@ -1,7 +1,7 @@
 #ifndef MINISCATTERVERSION_HH
 #define MINISCATTERVERSION_HH 1
 
-inline const char* const miniscatter_version = "1.3-3";
-inline const char* const miniscatter_date    = "May 10th 2024";
+inline const char* const miniscatter_version = "1.3-4";
+inline const char* const miniscatter_date    = "Nov 5th 2024";
 
 #endif
